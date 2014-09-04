32c49b6273aa0252ebbdf56bc03e05f766e61c63
drewtunney <bryantunney@gmail.com>
2014-09-04 06:59:40 -0700

    working facebook login

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-var app = angular.module("sampleApp", ["firebase"]);
+'use strict';
 
-app.controller("SampleCtrl", function($scope, $firebase) {
+
+var app = angular.module('sampleApp', ['firebase', 'ngRoute']);
+
+app.controller("SampleCtrl", function($scope, $firebase, 
+  $firebaseSimpleLogin) {
+
   var ref = new Firebase("https://popping-torch-3987.firebaseio.com/messages");
   var sync = $firebase(ref);
   $scope.messages = sync.$asArray();
+  console.log($scope.messages)
   $scope.addMessage = function(text) {
     $scope.messages.$add({text: text});
   }
+
+  var authClient = $firebaseSimpleLogin(ref);
+  // log user in using the Facebook provider for Simple Login
+
+  $scope.loginWithFacebook = function() {
+      authClient.$login('facebook').then(function(user) {
+      console.log(user)
+      $scope.user = user
+       console.log($scope.user.displayName)
+    }, function(error) {
+      console.error('Login failed: ' + error);
+    });
+  }
+
 });
+
+
+// app.config(function($routeProvider) {
+//     $routeProvider
+//     .when('/',
+//       {
+//         templateUrl: 'index.html',
+//         controller: 'SampleCtrl'
+//       })
+//     .when('/login', {
+//       templateUrl: 'views/login.html',
+//       controller: 'LoginCtrl'
+//     })
+
+// })
